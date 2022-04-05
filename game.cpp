15f51d030a43b39bc6c9a28827d3eaac55ea8513
laneinline <laneinline@gmail.com>
2022-04-05 17:58:31 +0300

    sShip basic movement

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 
 
 #include "Framework.h"
+#include <iostream>
 
 
 
@@ -9,18 +10,36 @@ class MyFramework : public Framework {
 
 public:
 
-	virtual void PreInit(int& width, int& height, bool& fullscreen)
+	Sprite* sShip = nullptr;
+	int posX = 100;
+	int	posY = 100;
+
+	int scrWidth;
+	int scrHeight;
+
+
+	virtual void PreInit(int& width, int& height, bool& fullscreen) 
 	{
-		width = 320;
-		height = 200;
+		width = 800;
+		height =600;
 		fullscreen = false;
 	}
 
-	virtual bool Init() {
+	virtual bool Init() { //runs automatically  on create obj
 
-		//drawTestBackground();
+		drawTestBackground();
 		//Sprite* mySprite = nullptr;
 		//mySprite = createSprite("data/big_asteroid.bmp");
+
+
+		getScreenSize(scrWidth, scrHeight);
+		std::cout << " scrWidth " << scrWidth << " scrHeight " << scrHeight << std::endl; 
+
+		posX = scrWidth / 2;
+		posY = scrHeight / 2;
+
+		sShip = createSprite("data/spaceship.png");
+		std::cout << " sShip position X: " << posX << " Y: " << posY << std::endl;
 
 		
 		return true;
@@ -33,40 +52,65 @@ public:
 	virtual bool Tick() {
 
 		drawTestBackground();
-		Sprite* mySprite = nullptr;
-		mySprite = createSprite("data/big_asteroid.png");
-		drawSprite(mySprite, 10, 10);
-   
+
+		drawSprite(sShip, posX, posY);
+		
+		//int w, h;
+		//getScreenSize(w,h);
+		//std::cout << " w: " << w << " h: " << h << std::endl;
+
 		return false;
 	}
 
 	virtual void onMouseMove(int x, int y, int xrelative, int yrelative) {
+		std::cout << " Mouse " << " _ " << " moved" << std::endl;
 
 	}
 
 	virtual void onMouseButtonClick(FRMouseButton button, bool isReleased) {
+		std::cout << " Mouse " << " Button " << " is clicked" << std::endl;
 
 	}
 
 	virtual void onKeyPressed(FRKey k) {
+		if (k == FRKey::UP) { 
+			std::cout << " Key " << " UP " << " is pressed" << std::endl; 
+			posY -= 25; 
+			std::cout << " sShip position X: " << posX << " Y: " << posY << std::endl;
+		}
+		if (k == FRKey::DOWN) { 
+			std::cout << " Key " << " DOWN " << " is pressed" << std::endl; 
+			posY += 25; 
+			std::cout << " sShip position X: " << posX << " Y: " << posY << std::endl;
+		}
+		if (k == FRKey::LEFT) { std::cout << " Key " << " LEFT " << " is pressed" << std::endl; posX -= 25; }
+		if (k == FRKey::RIGHT) { std::cout << " Key " << " RIGHT " << " is pressed" << std::endl; posX += 25; }
+	
+
+
+		
 	}
 
 	virtual void onKeyReleased(FRKey k) {
+		//std::cout << " Key " << " UP " << " is released" << std::endl;
 	}
 	
 	virtual const char* GetTitle() override
 	{
 		return "asteroids";
 	}
+
+
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	
+
 
 	MyFramework myFramework;
 	//drawSprite(mySprite, 10, 10);
-	myFramework.Init();
-	//myFramework.Tick();
+	//myFramework.Init();
+
+
 	return run(&myFramework);
 }
